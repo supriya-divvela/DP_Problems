4d9432fb3fade79aa3f9df9b84d1cc16ab6c1811
supriya-divvela <81727984+supriya-divvela@users.noreply.github.com>
2022-03-12 16:44:32 +0530

    Update fibanocci_memoization.py

--- fibanocci_memoization.py
+++ fibanocci_memoization.py
@@ -4,14 +4,14 @@ def fibanocci(n,mem={1:1,2:1}):
    x=fibanocci(n-1,mem)+fibanocci(n-2,mem)
    mem[n]=x
    return x
-print(fibanocci(int(input())))
+print(fibanocci(int(input())+1))
 
 
 '''
 ip:45
-op:1134903170
+op:1836311903
 
 
-ip:30
-op:39088169
+ip:38
+op:63245986
 '''
